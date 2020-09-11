b2f48439946c24ef72bd080bbf37da8c1f0af847
Svata Dedic <svatopluk.dedic@oracle.com>
2020-09-11 17:47:54 +0200

    Fixing positions in anno-processors. Tolerate enabled libs.graaljs so PAC works in core.

--- ide/gsf.testrunner/src/org/netbeans/modules/gsf/testrunner/TestCreatorProviderProcessor.java
+++ ide/gsf.testrunner/src/org/netbeans/modules/gsf/testrunner/TestCreatorProviderProcessor.java
@@ -51,13 +51,20 @@ public class TestCreatorProviderProcessor extends LayerGeneratingProcessor {
             f.stringvalue("instanceOf", TestCreatorProvider.class.getName());
             f.bundlevalue("displayName", registration.displayName());
             f.bundlevalue("identifier", registration.identifier());
-            if (registration.position() != -1 && registration.position() != Integer.MAX_VALUE) {
-                f.intvalue("position", registration.position());
+            int pos = registration.position();
+            if (pos != -1) {
+                if (pos == Integer.MAX_VALUE) {
+                    String n = f.getPath();
+                    int lsl = n.lastIndexOf("/");
+                    n = n.substring(lsl + 1);
+                    pos = 1000000 + (n.hashCode() % 100000);
+                }
+                f.intvalue("position", pos);
             }
             f.write();
         }
 
-        return true;
-    }
+        return true; 
+   }
     
 }
